2e34f7e70aa9ccfcdc9a19922f1044c962eb655e
mac411 <mcman351@gmail.com>
2021-12-12 01:54:32 -0800

    Create functionality to get robot pose from odometry data

--- scripts/obstacle_detector.py
+++ scripts/obstacle_detector.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+from numpy.core.numeric import roll
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 import numpy as np
 from point_cloud2 import pointcloud2_to_xyz_array
 from std_srvs.srv import Trigger
-from math import sqrt, atan2, cos, sin, floor
+from math import sqrt, atan2, cos, sin, asin, floor
 from angles import normalize, d2r, r2d
+from nav_msgs.msg import Odometry
+# from geometry_msgs.msg import Pose
 
 
 class ObstacleDetector(Node):
@@ -14,10 +17,14 @@ class ObstacleDetector(Node):
         super().__init__('obstacle_detector')
         self.xyz_points = None
         self.occ_grid = np.zeros((50, 50), dtype=np.bool)
+        self.position = None
+        self.orientation = None
         self.srv = self.create_service(
             Trigger, 'obstacle_update', self.obstacle_callback)
         self.laser_subscription = self.create_subscription(
             PointCloud2, 'laser/out', self.laser_callback, 10)
+        self.odom_sub = self.create_subscription(
+            Odometry, 'demo/odom_demo', self.odom_callback, 10)
         # self.obstacle_pub = self.create_publisher()
 
     def laser_callback(self, msg):
@@ -29,15 +36,24 @@ class ObstacleDetector(Node):
         # self.get_logger().info(np.str(poo[:1,...]))
         self.get_logger().info(np.str(self.xyz_points))
         self.get_logger().info(np.str(poo))
-        self.get_logger().info(np.str(np.argwhere(self.occ_grid)))
+        # self.get_logger().info(np.str(np.argwhere(self.occ_grid)))
+
+    def odom_callback(self, msg):
+        self.position = msg.pose.pose.position
+        self.orientation = msg.pose.pose.orientation
+        # self.get_logger().info(str(self.pose.position.x))
+        # self.get_logger().info(str(self.pose.position.y))
+        # self.get_logger().info(str(self.pose.position.z))
 
     def tester_fn(self):
-        sx = 0.834015
-        sy = -0.003286
-        st = -0.012722
-        d = sqrt((sx**2) + (sy**2))
-        t = 0.626893
-        robot_pose = np.array([-2.899256 + d*cos(t+st), -2.272856 + d*sin(t+st), t]) # Have to get this somehow
+        # sx = 0.834015
+        # sy = -0.003286
+        # st = -0.012722
+        # d = sqrt((sx**2) + (sy**2))
+        # t = 0.626893
+        robot_pose = np.array([0, 0, 0]) # Have to get this somehow
+        if self.position is not None:
+            robot_pose = np.array([self.position.x, self.position.y, self.euler_from_quaternion(self.orientation)[2]])
         robot_x = robot_pose[0]
         robot_y = robot_pose[1]
         robot_theta = robot_pose[2]
@@ -81,6 +97,23 @@ class ObstacleDetector(Node):
             #     self.xyz_points[i, 0]/sqrt((self.xyz_points[i, 0]**2) + (self.xyz_points[i, 1]**2))))
             ang = r2d(atan2(self.xyz_points[i, 1],self.xyz_points[i, 0]))
             self.xyz_points[i, 2] = d2r(normalize(ang, -180, 180)) # probably not necessary
+
+    def euler_from_quaternion(self,orientation):
+        x = orientation.x
+        y = orientation.y
+        z = orientation.z
+        w = orientation.w
+        t0 = +2.0*(w*x + y*z)
+        t1 = +1.0 - 2.0*(x*x + y*y)
+        r = atan2(t0, t1)
+        t2 = +2.0*(w*y - z*x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        p = asin(t2)
+        t3 = +2.0*(w*z + x*y)
+        t4 = +1.0 - 2.0*(y*y + z*z)
+        yaw = atan2(t3, t4)
+        return np.array([r, p, yaw])
 
 
 def main(args=None):
